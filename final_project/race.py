f0bd4b48a43eca0904dc450d2cd3daf408e177a0
Vivek Sourabh <viveksourabh2808@gmail.com>
2021-11-28 15:05:55 -0500

    Layout fixes
    Signed-off-by: Vivek Sourabh <viveksourabh2808@gmail.com>

--- final_project/race.py
+++ final_project/race.py
@@ -23,8 +23,6 @@ def load_data():
 
     return race_df, laps, driver_name
 
-
-
 def init():
     st.set_page_config(layout="wide")
     if 'race_year' not in st.session_state:
@@ -62,10 +60,11 @@ def update_race_first():
     if st.session_state.race:
         st.session_state.race = race_df[race_df.year == st.session_state['race_year']]['name'].unique().tolist()[0]
 
-row1_col1, row1_col2 = st.columns(2)
+dum1, row1_col1, row1_col2, dum2 = st.columns([1,2,4,1])
+# row1_col1 = st.container()
 
 row1_col1.selectbox("Select Year", race_df['year'].sort_values().unique().tolist(), on_change=handle_change_race_year, key='race_year')
-row1_col2.selectbox("Select Race", race_list, on_change=handle_change_race, key='race')
+row1_col1.selectbox("Select Race", race_list, on_change=handle_change_race, key='race')
 
 race_id = race_df[(race_df.year==st.session_state['race_year']) & (race_df.name==st.session_state['race'])]['raceId'].tolist()[0]
 
@@ -79,7 +78,9 @@ for index, row in driver_name.iterrows():
 
 total_laps = len(laps)
 
-col1, col2 = st.columns(2)
+# col1, col2 = st.columns(2)
+d1, col1, d2 = st.columns([1,6,1])
+col2 = st.container()
 
 if total_laps <= 400:
     col1.write('Not enough Data available select another race')
@@ -120,12 +121,12 @@ else:
                   type='buttons', showactive=False, y=0.15, x=1.15,
                       xanchor='right', yanchor='bottom',
                       buttons=[dict(label='Play', method='animate', args=[
-                          None, dict(frame=dict(duration=50, redraw=False),
+                          None, dict(frame=dict(duration=200, redraw=False),
                                     transition=dict(duration=0), fromcurrent=True, mode='immediate')
                       ])]
                   )])
     layout.update(xaxis =dict(range=[0, num_laps+1], autorange=False),
-              yaxis =dict(range=[0, len(driver_list)+1], autorange=False), height=800, width=800, colorway=random.sample(colors_list,  len(driver_list)))
+              yaxis =dict(range=[0, len(driver_list)+1], autorange=False), height=800, width=1000, colorway=random.sample(colors_list,  len(driver_list)))
     fig = go.Figure(data=traces, frames=frames, layout=layout)
     fig.update_yaxes(autorange="reversed")
     col1.plotly_chart(fig)
@@ -138,7 +139,7 @@ else:
     col2.write('\n')
     col2.write('\n')
     col2.write('\n')
-    col2.dataframe(race_results.reset_index().drop('index', axis=1), height=1000)
+    row1_col2.dataframe(race_results.reset_index().drop('index', axis=1))
 
 
 
